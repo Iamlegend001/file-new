f79a7d71e159c0d2e2d1452be80e62c64ed84680
Arka <arkapravasantra17@gmail.com>
2025-06-08 23:18:59 +0530

    Get ready to implement redux tool kit

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,21 +1,42 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { nanoid } from "nanoid";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-
-
+  const { register, handleSubmit, reset } = useForm();
+  const submitHandler = (user) => {
+    
+    console.log(user);
+  };
   return (
-    <form className="flex flex-col w-1/2">
-      <input
-        className=" outline-0 border-b p-2 text-4xl mb-3"
-        type="text"
-        placeholder="Name"
-      />
-      <input
+    <form
+      onSubmit={handleSubmit(submitHandler)}
+      className="flex flex-col w-1/2 gap-2 justify-center h-screen"
+    >
 
-        className=" outline-0 border-b p-2 text-4xl mb-3"
-        type="text"
-        placeholder="Name"
+      <input
+        className=" outline-0 border-b p-2 text-2xl mb-3"
+        type="email"
+        placeholder="email"
+        {...register("email")}
       />
+      <input
+        className=" outline-0 border-b p-2 text-2xl mb-3"
+        type="password"
+        placeholder="password"
+        {...register("password")}
+      />
+      <button className="item-center justify-center bg-white text-black w-20 h-10 rounded-2xl">
+        Login
+      </button>
+     <div className="flex gap-2">
+     <p>Don't have a Account ?,</p>
+      <Link to="/register" className="text-red-700">
+        {" "}
+        Click Here
+      </Link>
+     </div>
     </form>
   );
 };
--- Frontend/src/Pages/Register.jsx
+++ Frontend/src/Pages/Register.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
+import { nanoid } from "nanoid";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const { register, handleSubmit, reset,formState:{errors} } = useForm();
+  const RegisterHandler = (user) => {
+    user.id = nanoid();
+    console.log(user);
+  };
   return (
-    <div>Register</div>
-  )
-}
+    <form
+      onSubmit={handleSubmit(RegisterHandler)}
+      className="flex flex-col w-1/2 gap-2 justify-center h-screen"
+    >
+      <input
+        className=" outline-0 border-b p-2 text-2xl mb-3"
+        type="text"
+        placeholder="username"
+        {...register("username",{required:"Required"})}
+      />
+      <small className='text-red-700'>{errors.username && errors.username.message}</small>
+      <input
+        className=" outline-0 border-b p-2 text-2xl mb-3"
+        type="email"
+        placeholder="email"
+        {...register("email")}
+      />
+      <input
+        className=" outline-0 border-b p-2 text-2xl mb-3"
+        type="password"
+        placeholder="password"
+        {...register("password")}
+      />
+      <button className="item-center justify-center bg-white text-black w-20 h-10 rounded-2xl">
+        Register
+      </button>
+     <div className="flex gap-2">
+     <p>Already have an Account ?,</p>
+      <Link to="/login" className="text-red-700">
+        {" "}
+        Click Here
+      </Link>
+     </div>
+    </form>
+  );
+};
+
 
 export default Register
